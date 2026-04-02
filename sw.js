f3d2874f0bd21cc707b512016c1d72d2a02a6b81
fildookubica-commits <fildoo.kubica@gmail.com>
2026-04-02 20:39:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⚠️ VERZIA — zmeň toto číslo pri každom nasadení novej verzie
-const CACHE_NAME = 'dochadzka-mars-v19';
+const CACHE_NAME = 'dochadzka-mars-v20';
 
 const urlsToCache = [
   '/navstevy-app/',

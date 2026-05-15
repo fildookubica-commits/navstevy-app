8752c1f000986eb36d5d687a5f0bf0d926afd5c6
fildookubica-commits <fildoo.kubica@gmail.com>
2026-05-15 06:40:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ⚠️ VERZIA — zmeň toto číslo pri každom nasadení novej verzie
-const CACHE_NAME = 'FKAPP-MARS-015';
+const CACHE_NAME = 'FKAPP-MARS-016';
 
 const urlsToCache = [
   '/navstevy-app/',
